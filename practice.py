2b1284f32567de20a410127f47d7696adb87b991
K-yndon <131963244+K-yndon@users.noreply.github.com>
2023-05-19 10:46:43 +0100

    Update practice.py

--- practice.py
+++ practice.py
@@ -20,7 +20,6 @@ hide_st_style = """
                 #MainMenu {visibility: hidden;}
                 footer {visibility: hidden;}
                 header {visibility: hidden;}
-                .viewerBadge_link__1S137 {visibility: hidden;}
                 </style>
                 """
 st.markdown(hide_st_style, unsafe_allow_html=True)
